31e14f6a934639a3f74c5ebdc857f7a534a466e5
onyx6838 <giang.mink@gmail.com>
2021-11-04 00:31:34 +0700

    valid FE, auditing BE

--- FE/assets/js/client/test.js
+++
@@ -1,7 +0,0 @@
-const fullName = 'Đoàn Minh Giang';
-
-var firstName = fullName.split(' ').slice(0, -1).join(' ');
-var lastName = fullName.split(' ').slice(-1).join(' ');
-
-console.log(firstName);
-console.log(lastName);
